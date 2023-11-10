e0d63fa8b910f6dc9ec1937b32e07b537f527174
Albert Puigsech Galicia <albert@puigsech.com>
2023-11-10 18:55:46 +0100

    initial commit

---
+++ components/Web3Provider.vue
@@ -0,0 +1,102 @@
+<script setup lang="ts">
+import { useDappStore } from '@/stores/useDappStore'
+import { storeToRefs } from 'pinia'
+import { ethers } from 'ethers'
+
+import { MetaMaskConnector, useWalletStore } from '@vue-dapp/core'
+import { Board } from '@vue-dapp/vd-board'
+import { WalletConnectConnector } from '@vue-dapp/walletconnect'
+
+const dappStore = useDappStore()
+const { isConnected, user } = storeToRefs(dappStore)
+
+const { onActivated, onChanged, onDeactivated } = useWalletStore()
+
+onActivated(async ({ address, provider }) => {
+	const ethersProvider = new ethers.BrowserProvider(provider)
+	const signer = await ethersProvider.getSigner()
+	const network = await ethersProvider.getNetwork()
+
+	dappStore.setUser({
+		address,
+		signer,
+		chainId: Number(network.chainId),
+	})
+})
+
+onChanged(async ({ address, provider }) => {
+	const ethersProvider = new ethers.BrowserProvider(provider)
+	const signer = await ethersProvider.getSigner()
+	const network = await ethersProvider.getNetwork()
+
+	dappStore.setUser({
+		address,
+		signer,
+		chainId: Number(network.chainId),
+	})
+})
+
+onDeactivated(() => {
+	dappStore.resetUser()
+})
+
+const connectors = [
+	new MetaMaskConnector(),
+	new WalletConnectConnector({
+		projectId: '3f3c98042b194264687bf59e104c534a',
+		chains: [1],
+		showQrModal: true,
+		qrModalOptions: {
+			themeMode: 'dark',
+			themeVariables: undefined,
+			chainImages: undefined,
+			desktopWallets: undefined,
+			walletImages: undefined,
+			mobileWallets: undefined,
+			enableExplorer: true,
+			explorerAllowList: undefined,
+			tokenImages: undefined,
+			privacyPolicyUrl: undefined,
+			explorerDenyList: undefined,
+			termsOfServiceUrl: undefined,
+		},
+	}),
+]
+
+function connectErrorHandler(err: any) {
+	console.error('ConnectError', err)
+}
+function autoConnectErrorHandler(err: any) {
+	console.error('AutoConnectError', err)
+}
+
+watch(
+	() => dappStore.network,
+	() => {
+		console.log('Network changed')
+	},
+)
+
+watch(isConnected, () => {
+	if (isConnected.value) {
+		console.log('Wallet connected', user.value)
+	} else {
+		console.log('Wallet disconnected', user.value)
+	}
+})
+</script>
+
+<template>
+	<div>
+		<slot />
+
+		<Board
+			:connectors="connectors"
+			dark
+			:autoConnectErrorHandler="autoConnectErrorHandler"
+			:connectErrorHandler="connectErrorHandler"
+		/>
+	</div>
+</template>
+
+<style lang="scss"></style>
