3ffdce8d6d576d28a67da16590f8cca2899da634
Albert Puigsech <albert@puigsech.com>
2023-12-01 09:38:28 +0900

    refactor useOpenLottoStore

--- stores/useOpenLottoStore.ts
+++ stores/useOpenLottoStore.ts
@@ -1,36 +1,42 @@
 import { defineStore } from 'pinia';
+import { ref, computed } from 'vue';
 import { JsonRpcProvider } from 'ethers';
 import { useDappStore } from '@/stores/useDappStore';
 import { OpenLotto, LotteryItem } from '@apuigsech/openlotto-bindings';
 
 const openLottoAddress = '0x21FBd49FfdDc52AB3e088813E48B2C3BB06A4528';
-const dummyOperatorAddress = '0x32049dCEB926f5Dbda4e4215ce603e8252C69B21';
 
-export type OpenLottoState = {
-    lotteryList: LotteryItem[];
-}
+export const useOpenLottoStore = defineStore('openlotto', () => {
+    const dappStore = useDappStore();
 
-export const useOpenLottoStore = defineStore('openlotto', {
-    state: (): OpenLottoState => ({
-        lotteryList: [],
-    }),
-    getters: {
-        provider() {
-            const dappStore = useDappStore();
-            if (dappStore.isConnected) {
-                return toRaw(dappStore.signer);
-            } else {
-                return toRaw(dappStore.provider);
-            }
-        },
-        contract() {
-            return new OpenLotto(openLottoAddress, this.provider);
-        },
-        GetLotteryList: (state) => state.lotteryList,
-    },
-    actions: {
-        async ReadLottery(id) {
-            return this.contract.ReadLottery(id);
-        }
+    const lotteryMap = ref(new Map<Number, LotteryItem>());
+
+    const provider = computed(() => {
+        return dappStore.isConnected ? dappStore.signer : dappStore.provider;
+    });
+
+    const openlotto = computed(() => {
+        return new OpenLotto(openLottoAddress, provider.value);
+    });
+
+    async function FetchLottery(id: Number) {
+        console.log("FetchLottery", id);
+        let lottery = await openlotto.value.ReadLottery(id);
+        lotteryMap.value.set(id, lottery);
+        return lottery;
     }
+
+    async function ReadLottery(id: Number) {
+        console.log("ReadLottery", id);
+        let lottery = lotteryMap.value.get(id);
+        if (!lottery) {
+            lottery = await FetchLottery(id);
+        }
+        return lottery;
+    }
+
+    return {
+        FetchLottery,
+        ReadLottery,
+    };
 });
