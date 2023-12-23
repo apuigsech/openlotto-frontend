21db9cf6e39197b023718b3033dcf7b2c58423e8
Albert Puigsech <albert@puigsech.com>
2023-12-23 12:14:04 +0100

    add blockNumber on the header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,12 @@
 <script lang="ts" setup>
+	const blockNumber = ref(0);
+
+    useBlockNumber({
+        watch: true,
+		onBlock(n) {
+			blockNumber.value = Number(n);
+		}
+    })
 </script>
 
 <template>
@@ -7,6 +15,7 @@
 			<v-app-bar-title>
 				<NuxtLink to="/">OpenLotto</NuxtLink>
 			</v-app-bar-title>
+			#{{ blockNumber }}
             <wallet />
 		</v-app-bar>
 		<v-main>
